7aace590062e79e7539f2f11ad4e2e403d42def2
Sana Assim <sanaassim1@gmail.com>
2023-08-29 05:39:10 -0700

    Measure the size of a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measure the size of a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to measure the size
+ *
+ * Return: the size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size;
+
+	if (!tree)
+		return (0);
+
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+}
