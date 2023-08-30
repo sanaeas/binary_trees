eef446d0f0b0978e8f961ec61fa231e014633e22
Sana Assim <sanaassim1@gmail.com>
2023-08-30 10:51:43 -0700

    Build a Binary Search Tree from an array

---
+++ 112-array_to_bst.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - build a Binary Search Tree from an array
+ *
+ * @array: a pointer to the first element of the array to be converted
+ * @size: the number of element in the array
+ *
+ * Return: a pointer to the root node of the created BST
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *root = NULL;
+
+	if (!array || size == 0)
+		return (NULL);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		bst_insert(&root, array[i]);
+	}
+
+	return (root);
+}
