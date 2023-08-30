05ec3a14cfb0b15871d8383a163a578463818267
Sana Assim <sanaassim1@gmail.com>
2023-08-30 11:00:07 -0700

    Fix betty

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -11,11 +11,12 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	bst_t *root = NULL;
+	size_t i;
 
 	if (!array || size == 0)
 		return (NULL);
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		bst_insert(&root, array[i]);
 	}
